738d71b72ec4b8263b6b81c675a2f37fd8393c70
Yokozuna59 <u.yokozuna@gmail.com>
2022-04-01 18:10:51 +0300

    add new line

--- delay.py
+++ delay.py
@@ -8,5 +8,5 @@ def time_delay(refresh_time):
     for i in range(1, refresh_time+1):
         stdout.write("\r\033[46mNext Refresh: [{}{}] | {}%".format("#" * i, " " * (refresh_time - i), str(round(i/refresh_time * 100, 2))))
         sleep(1)
-    stdout.write("\033[0m")
+    stdout.write("\033[0m\n")
     stdout.flush()
