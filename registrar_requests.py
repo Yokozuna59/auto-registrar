c6cb0c8b6ea154d33b3c0c2ae0aee7bd8cda7f9e
Yokozuna59 <u.yokozuna@gmail.com>
2022-04-02 16:51:24 +0300

    make all requests here

--- registrar_requests.py
+++ registrar_requests.py
@@ -4,26 +4,22 @@ from requests import get
 # import BeautifulSoup to parse the content of the url
 from bs4 import BeautifulSoup
 
-# import color_choices function from colorful_terminal.py file to show the choices of the user
-from colorful_terminal import color_choices
+# import color_choices and color_input functions to print colored text
+from colorful_terminal import color_choices, color_input, tcolor
 
-# import requests_not_200 function from errors.py file to check if the website is working or not
-# import and no_internet_connection function from errors.py file to check if there is internet connection or not
+# import no_internet_connection, request_not_200, input_not_digit and number_out_of_range functions to show the error messages
 import errors
-
-# import get_user_input function from user_inputs.py file to get the user input
-from user_inputs import get_user_input
 
 def get_requests(url):
     try:
         registrar_request = get(url)
     except:
-        return errors.no_internet_connection(url)
+        return get_elements(errors.no_internet_connection(url))
 
     if (registrar_request.status_code != 200):
         errors.request_not_200(url)
     else:
-        return registrar_request.content
+        return get_elements(registrar_request.content)
 
 def get_elements(content):
     soup = BeautifulSoup(content, "html.parser")
@@ -31,6 +27,8 @@ def get_elements(content):
     user_inputs = []
     for i in ("course_term_code", "course_dept_code"):
         element = soup.find(id=i)
+        if (element is None):
+            return soup
         options = element.find_all("option")[1::]
 
         elements_dict = {}
@@ -38,7 +36,14 @@ def get_elements(content):
             elements_dict[option.text] = option["value"]
 
         color_choices(elements_dict)
-        user_input = int(get_user_input(elements_dict))
-        user_inputs.append(list(elements_dict.values())[user_input - 1])
+        user_input = color_input("[*] - Enter number: ", tcolor.OKGREEN)
+
+        if (user_input.isdigit()):
+            if (not len(elements_dict) > 0 and not len(elements_dict) >= int(user_input)):
+                errors.number_out_of_range()
+
+            user_inputs.append(list(elements_dict.values())[int(user_input) - 1])
+        else:
+            errors.input_not_int(user_input)
 
     return user_inputs
