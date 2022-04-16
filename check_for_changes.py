6698fe1d8e70d311c0b23eaa19ecef8898a55fe1
Yokozuna59 <u.yokozuna@gmail.com>
2022-04-16 06:31:36 +0300

    add filter for None

--- check_for_changes.py
+++ check_for_changes.py
@@ -20,20 +20,21 @@ def check_for_change(content, search_user_input, driver_path):
             # one or more CRNs are not in the department and term specified
             pass
     else:
-        for i in content_json:
-            crn = i["crn"]
-            course_name = i["course_number"]
-            section = i["section_number"]
-            available_seats = i["available_seats"]
-            waiting_list_count = i["waiting_list_count"]
-            class_type = i["class_type"]
+        found_elements = filter(lambda i: int(i["available_seats"]) > 0 or int(i["waiting_list_count"]) > 0,  content_json)
+        for i in found_elements:
+            crn                 = i["crn"]
+            course_name         = i["course_number"]
+            section             = i["section_number"]
+            available_seats     = i["available_seats"]
+            waiting_list_count  = i["waiting_list_count"]
+            class_type          = i["class_type"]
 
             if (available_seats > 0):
                 color_print(f"[+] - {course_name}-{section},", tcolor.OKGREEN, " ")
                 color_print(f"Type: {class_type}", tcolor.LEC if class_type == "LEC" else tcolor.LAB if class_type == "LAB" else tcolor.RES, ", ")
                 color_print(f"Available Seats: {available_seats}, Waiting List: {waiting_list_count}", tcolor.OKGREEN, ", ")
                 color_print(f"CRN: {crn}", tcolor.HEADER)
-            elif (waiting_list_count > 0)   :
+            elif (waiting_list_count > 0):
                 color_print(f"[-] - {course_name}-{section},", tcolor.WARNING, " ")
                 color_print(f"Type: {class_type}",  tcolor.LEC if class_type == "LEC" else tcolor.LAB if class_type == "LAB" else tcolor.RES, ", ")
                 color_print(f"Available Seats: {available_seats}, Waiting List: {waiting_list_count}", tcolor.WARNING, ", ")
