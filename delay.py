8da8fd9433c775c3ff22b9cf810473359f72bf11
Yokozuna59 <u.yokozuna@gmail.com>
2022-05-06 22:55:41 +0300

    edit first print

--- delay.py
+++ delay.py
@@ -14,11 +14,14 @@ def time_delay(refresh_time:int):
     stdout.write("\x1b[s")
     stdout.write("\x1b[?25l")
     for i in range(1, refresh_time+1):
-        sleep(1)
+        stdout.write("\x1b[u")
         terminal_columns = get_terminal_size()[0]-15
+        if (i==1):
+            stdout.write("\x1b[0mNext Refresh: |{}|".format(" " * (terminal_columns-1)))
+            stdout.write("\x1b[u")
+        sleep(1)
         stdout.write("\x1b[2K")
         stdout.write("\x1b[39mNext Refresh: |{}".format("█" * (floor(terminal_columns * (i/refresh_time))-1)))
         stdout.write("{}".format(" " * ceil(terminal_columns * (refresh_time - i)/refresh_time)) + "|" if (terminal_columns-i)!=0 else "|")
-        stdout.write("\x1b[u")
         stdout.flush()
     stdout.write("\033[0m\n")
