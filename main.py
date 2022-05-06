0066626a1d48839cbfcd415e8d7ed4eb9a5ea900
Yokozuna59 <u.yokozuna@gmail.com>
2022-05-06 21:35:51 +0300

    add some docs and driver_path and some more

--- main.py
+++ main.py
@@ -1,35 +1,47 @@
 # import setrecursionlimit to set the recursion limit to 100000
 from sys import setrecursionlimit
 
-# import check_config to check if user has configurate yet or not
-from config import check_config
+# import check_configuration to check if user has configurated yet or not
+from config import check_configuration
 
 # import get_requests to get the content of the url
 from registrar_requests import get_requests
 
-# import time_delay to make the script wait
-from delay import time_delay
-
 # import get_search_input function to get the user input for the search
 from user_search import get_search_input
 
-# import check_platform to get driver path
+# import check_platform to get driver's path
 from check_platforms import check_platform
 
-# import check_for_change to check if section has changed
+# import check_for_change to check if sections has changed
 from check_for_changes import check_for_change
 
-def main():
+# import time_delay to make the script wait
+from delay import time_delay
+
+def main() -> None:
+    """
+    This is the main and start function of this project.
+    """
+
+    # set recursion limit to 1000000
     setrecursionlimit(100000)
-    configurations = check_config()
-    registrar_user_input = get_requests("https://registrar.kfupm.edu.sa/courses-classes/course-offering/")
+
+    # get config file as dict type
+    configuration = check_configuration()
+
+    # get term and department as list
+    registrar_user_input = get_requests(url="https://registrar.kfupm.edu.sa/courses-classes/course-offering/")
+
     search_user_input = get_search_input()
-    driver_path = check_platform(configurations["browser"])
+
+    driver_path = check_platform(browser=configuration["browser"])
 
     while True:
-        content = get_requests(f"https://registrar.kfupm.edu.sa/api/course-offering?term_code={registrar_user_input[0]}&department_code={registrar_user_input[1]}")
+        content = get_requests(url="https://registrar.kfupm.edu.sa/api/course-offering?term_code={}&department_code={}".format(registrar_user_input[0], registrar_user_input[1]))
         check_for_change(content, search_user_input, driver_path)
-        time_delay(configurations["delay"])
+        time_delay(refresh_time=configuration["delay"])
 
 if __name__ == "__main__":
+    # run the main function
     main()
