79e8c17ac94ace1157877becc5b2ceff4ceec240
Yokozuna59 <u.yokozuna@gmail.com>
2022-05-09 23:56:33 +0300

    edit url variable name and add docs for the functions

--- registrar_requests.py
+++ registrar_requests.py
@@ -4,35 +4,37 @@ from requests import get
 # import BeautifulSoup to parse the content of the url
 from bs4 import BeautifulSoup
 
-# import Color_cli and Questions classes to print colorful text and ask user for input
-from cli import Color_cli, Questions
+# import cli_colors and Questions classes to print colorful text and ask user for input
+from cli import cli_colors, Questions
 
 # import time_delay to make the script wait
 from delay import time_delay
 
-def get_requests(url:str) -> dict:
+def get_requests(request_url: str) -> dict:
     """
-    This function do a get request by `url` and return value as dict
+    Do a get request by `url`.\n
+    return it value as `dict` type.
     """
 
     while True:
         try:
-            registrar_request = get(url)
+            registrar_request = get(url=request_url)
             break
         except:
-            Color_cli.colorful_print(text="\n! You don't have internet connection, the script will check for sections every 10s..", text_color=Color_cli.BRIGHT_RED)
+            cli_colors.colorful_print(text_string="\n! Sorry, you currently don't have internet connection! the script will check in 10 seconds.", text_color=cli_colors.BRIGHT_RED)
             time_delay(10)
 
     if (registrar_request.status_code != 200):
-        Color_cli.colorful_print(text="! The website isn't working for the time being, the script will check every 60s..", text_color=Color_cli.BRIGHT_RED)
+        cli_colors.colorful_print(text_string="! Sorry, the website isn't working currently! the script will check in 60 seconds", text_color=cli_colors.BRIGHT_RED)
         time_delay(60)
-        return get_requests(url)
+        return get_requests(request_url=request_url)
     else:
-        return get_elements(registrar_request.content)
+        return get_elements(content=registrar_request.content)
 
 def get_elements(content) -> list:
     """
-    This function get terms and departments of the request
+    Get terms and departments of the request.\n
+    return it as a `list` type.
     """
 
     soup = BeautifulSoup(content, "html.parser")
