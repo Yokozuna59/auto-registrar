6f002ea14e33f34005fb9fe7ef37a2ccab30d058
Yokozuna59 <u.yokozuna@gmail.com>
2022-05-29 16:43:16 +0300

    get the running file and edit path for windows and check if has drivers

--- src/config.py
+++ src/config.py
@@ -1,11 +1,6 @@
-# import path and getcwd to check if user has runed the program before
-from os import path, getcwd
-
-# import loads and dump to load and dump the json data
+from os import path, listdir
 from json import loads, dump
-
-# import cli_colors ans Questions classes to print colorful text and ask user for input
-from cli import ColorfulText, Questions
+from cli import colorful_text, AnsiEscapeCodes, Questions
 
 def check_configurations() -> dict:
     """
@@ -13,37 +8,42 @@ def check_configurations() -> dict:
     If he did, return it as `dict` type.
     """
 
-    # get current path
-    current_path = getcwd()
-    if ("src" not in current_path):
-        # print error messege and stop the script
-        ColorfulText.colorful_print(text_string="! Sorry, you are in the wrong directory.", text_color=ColorfulText.BRIGHT_RED)
-        ColorfulText.colorful_print(text_string="Use `cd` command to change directory.", text_color=ColorfulText.BRIGHT_CYAN)
-        exit()
+    PROJECT_PATH =  __file__.replace("src/config.py", "").replace("src\config.py", "")
+    CONFIG_PATH = f"{PROJECT_PATH}.config.json"
+    DRIVERS_PATH = f"{PROJECT_PATH}/drivers" if "/" in PROJECT_PATH else f"{PROJECT_PATH}\drivers"
 
-    if (not path.exists(current_path.replace("src", ".config.json"))):
+    if (not path.exists(CONFIG_PATH)):
         # print error messege and stop the script
-        ColorfulText.colorful_print(text_string="! Sorry, you have run the `install.sh` file to run the script!", text_color=ColorfulText.BRIGHT_RED)
-        exit()
+        with open(CONFIG_PATH, 'w') as f:
+            f.write('{"configuration": null, "alarm": "alarms/default-alarm.mp3", "banner": 9, "browser": "chrome", "delay": 60, "interface": "cli", "passcode": null, "username": null}')
 
     # open config file
-    no_src_path = current_path.replace("src", "")
-    with open(f"{no_src_path}.config.json", 'r') as f:
+    with open(CONFIG_PATH, 'r') as f:
         # read and loads config file
         config_file = loads(f.read())
-    browser = config_file["browser"]
+        browser = config_file["browser"]
+
+    if (not path.exists(DRIVERS_PATH)):
+        colorful_text(text_string="! Sorry, you haven't ran `install.sh` or `install.ps1` files, or you haven't installed the drivers yet, or haven't created the driver folder!", text_color=AnsiEscapeCodes.RED)
+        colorful_text(text_string="! Since you don't have drivers, you won't be able to registrar for courses.", text_color=AnsiEscapeCodes.YELLOW)
+        config_file['browser'] = None
+    else:
+        # FIXME fix the drivers
+        DRIVERS = sorted(list(listdir(DRIVERS_PATH)))
+
     # check if user has configured yet or not
     if (config_file['configuration'] == None):
         # print error messege
-        ColorfulText.colorful_print(text_string="! Sorry, you haven't configured yet.", text_color=ColorfulText.BRIGHT_RED)
+        colorful_text(text_string="! Sorry, you haven't configured yet.", text_color=AnsiEscapeCodes.RED)
 
         # Ask user if he wants to config now or not
         bool_answer = Questions.bool_question(question="Do you want to configurate now")
 
         # if True ask 5 question and config the file
         if (bool_answer == True):
-            config_file = configuring(file_path=no_src_path, config_file=config_file)
-    config_file["driver"] = "{}{}".format(f"{no_src_path}/driver/", "chromedriver" if browser == "chrome" else "geckodriver")
+            config_file = configuring(file_path=CONFIG_PATH, config_file=config_file)
+    if (config_file['browser'] != None):
+        config_file['browser'] = "{}{}{}".format(DRIVERS_PATH, "/" if "/" in DRIVERS_PATH else "\\", DRIVERS[0] if browser == "chrome" else DRIVERS[1])
     return config_file
 
 def configuring(file_path: str, config_file: str) -> dict:
@@ -52,7 +52,7 @@ def configuring(file_path: str, config_file: str) -> dict:
     If he answered them, return them as `dict` type.
     """
 
-    with open(f"{file_path}.config.json", 'r') as f:
+    with open(file_path, 'r') as f:
         config_file = loads(f.read())
 
     default_banner = Questions.dict_question(question="Select default banner for registration", choices={"Banner 8":8, "Banner 9":9})
@@ -75,7 +75,7 @@ def configuring(file_path: str, config_file: str) -> dict:
 
     config_file["configuration"] = "configured"
 
-    with open(f"{file_path}.config.json", "w") as f:
+    with open(file_path, "w") as f:
         dump(config_file, f)
 
     return config_file
