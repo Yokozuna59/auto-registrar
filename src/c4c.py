32a4b7f896c28ca00a51162e61436a2dfc789c09
Yokozuna59 <u.yokozuna@gmail.com>
2022-06-17 16:13:05 +0300

    add TODO edit time chioce

--- src/c4c.py
+++ src/c4c.py
@@ -1,34 +1,34 @@
 from sys import stdout
 from cli import AnsiEscapeCodes
 
+ALL_COLOERS = {
+    "COP": "\x1b[48;2;92;148;13m",
+    "DIS": "\x1b[48;2;201;42;42m",
+    "FLD": "\x1b[48;2;33;37;41m",
+    "IND": "\x1b[48;2;33;37;41m",
+    "LAB": "\x1b[48;2;95;61;196m",
+    "LEC": "\x1b[48;2;24;100;171m",
+    "LLB": "\x1b[48;2;33;37;41m",
+    "LPJ": "\x1b[48;2;33;37;41m",
+    "MR": "\x1b[48;2;33;37;41m",
+    "PRJ": "\x1b[48;2;217;72;15m",
+    "REC": "\x1b[48;2;166,30,77m",
+    "RES": "\x1b[48;2;201;42;42m",
+    "SEM": "\x1b[48;2;8;127;91m",
+    "SLB": "\x1b[48;2;33;37;41m",
+    "ST": "\x1b[48;2;33;37;41m",
+    "STD": "\x1b[48;2;33;37;41m",
+    "THS": "\x1b[48;2;54;79;199m"
+}
+
 def check_for_changes(content: dict, search_input: dict, configurations: str) -> None:
     """
     Checks if there is available seats in courses.\n
     after checking, return `None`.
     """
 
-    content_json = content["data"]
-    all_colors = {"COP":"\x1b[48;2;92;148;13m",
-                  "DIS":"\x1b[48;2;201;42;42m",
-                  "FLD":"\x1b[48;2;33;37;41m",
-                  "IND":"\x1b[48;2;33;37;41m",
-                  "LAB":"\x1b[48;2;95;61;196m",
-                  "LEC":"\x1b[48;2;24;100;171m",
-                  "LLB":"\x1b[48;2;33;37;41m",
-                  "LPJ":"\x1b[48;2;33;37;41m",
-                  "MR":"\x1b[48;2;33;37;41m",
-                  "PRJ":"\x1b[48;2;217;72;15m",
-                  "REC":"\x1b[48;2;166,30,77m",
-                  "RES":"\x1b[48;2;201;42;42m",
-                  "SEM":"\x1b[48;2;8;127;91m",
-                  "SLB":"\x1b[48;2;33;37;41m",
-                  "ST":"\x1b[48;2;33;37;41m",
-                  "STD":"\x1b[48;2;33;37;41m",
-                  "THS":"\x1b[48;2;54;79;199m"
-                 }
-
     if (search_input == None):
-        found_elements = filter(lambda x: int(x["available_seats"]) > 0 or int(x["waiting_list_count"]) > 0,  content_json)
+        found_elements = filter(lambda x: int(x["available_seats"]) > 0 or int(x["waiting_list_count"]) > 0,  content)
         if (configurations["interface"] == "cli"):
             for index, element in enumerate(found_elements):
                 crn                 = element["crn"]
@@ -48,11 +48,11 @@ def check_for_changes(content: dict, search_input: dict, configurations: str) ->
                     continue
 
                 stdout.write(f"{colors}[{signs}] - {course_name}-{section}, ")
-                stdout.write(f"Type:{AnsiEscapeCodes.RESET} {all_colors[class_type]}{class_type}{AnsiEscapeCodes.RESET}{colors}, ")
+                stdout.write(f"Type:{AnsiEscapeCodes.RESET} {ALL_COLOERS[class_type]}{class_type}{AnsiEscapeCodes.RESET}{colors}, ")
                 stdout.write(f"Available Seats: {available_seats}, Waiting List: {waiting_list_count} ")
                 stdout.write("CRN:{} {}{}{}\n".format(AnsiEscapeCodes.RESET, AnsiEscapeCodes.LIGHT_BLUE if index%2==0 else AnsiEscapeCodes.LIGHT_MAGENTA, crn, AnsiEscapeCodes.RESET))
     else:
-        found_elements = filter(lambda x: int(x["available_seats"]) > 0 or int(x["waiting_list_count"]) > 0,  content_json)
+        found_elements = filter(lambda x: int(x["available_seats"]) > 0 or int(x["waiting_list_count"]) > 0,  content)
         if (configurations["interface"] == "cli"):
             for i in search_input:
                 if (i == "section"):
@@ -67,6 +67,7 @@ def check_for_changes(content: dict, search_input: dict, configurations: str) ->
                     found_elements = list(filter(lambda x: x["instructor_name"] in search_input["instructor_name"], found_elements))
                 elif (i == "class_days"):
                     found_elements = list(filter(lambda x: x["class_days"] in search_input["class_days"], found_elements))
+                # TODO edit time chioce
                 # elif (i == "time"):
                 #     (filter(lambda x: x["building"] in search_user_input["building"], found_elements))
                 elif (i == "building"):
@@ -98,7 +99,7 @@ def check_for_changes(content: dict, search_input: dict, configurations: str) ->
 
                 try:
                     stdout.write(f"{colors}[{signs}] - {course_name}-{section}, ")
-                    stdout.write(f"Type:{AnsiEscapeCodes.RESET} {all_colors[class_type]}{class_type}{AnsiEscapeCodes.RESET}{colors}, ")
+                    stdout.write(f"Type:{AnsiEscapeCodes.RESET} {ALL_COLOERS[class_type]}{class_type}{AnsiEscapeCodes.RESET}{colors}, ")
                     stdout.write(f"Available Seats: {available_seats}, Waiting List: {waiting_list_count} ")
                     stdout.write("CRN:{} {}{}{}\n".format(AnsiEscapeCodes.RESET, AnsiEscapeCodes.LIGHT_BLUE if index%2==0 else AnsiEscapeCodes.LIGHT_MAGENTA, crn, AnsiEscapeCodes.RESET))
                 except:
