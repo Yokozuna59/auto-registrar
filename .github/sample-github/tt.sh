184b588b6c72ca5f130a0c43181b7cef024c4618
Yokozuna59 <u.yokozuna@gmail.com>
2022-08-11 16:59:24 +0300

    add sample github file
    Signed-off-by: Yokozuna59 <u.yokozuna@gmail.com>

---
+++ .github/sample-github/tt.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# Copyright (c) 2013 Martin Seeler
+# SPDX-License-Identifier: MIT
+set -eu
+
+# destination of the final changelog file
+OUTPUT_FILE=CHANGELOG.md
+
+# generate the changelog
+git --no-pager log --no-merges --format="### %s%d%n>%aD%n%n>Author: %aN (%aE)%n%n>Commiter: %cN (%cE)%n%n%b%n%N%n" > $OUTPUT_FILE
+
+# prevent recursion!
+# since a 'commit --amend' will trigger the post-commit script again
+# we have to check if the changelog file has changed or not
+res=$(git status --porcelain | grep -c ".\$OUTPUT_FILE$")
+if [ "$res" -gt 0 ]; then
+  git add $OUTPUT_FILE
+  git commit --amend
+  echo "Populated Changelog in $OUTPUT_FILE"
+fi
