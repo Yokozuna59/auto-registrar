129ce594263a7b99d807bffbbe4497f05ac6a641
Yokozuna59 <u.yokozuna@gmail.com>
2022-08-14 05:35:56 +0300

    udpate __main__.py with better calling for main
    Signed-off-by: Yokozuna59 <u.yokozuna@gmail.com>

--- auto_registrar/__main__.py
+++ auto_registrar/__main__.py
@@ -1,1 +1,10 @@
-from . import main
+from os import system
+
+from .main import main
+
+if __name__ == "__main__":
+    # Enhance ansi colors in windows
+    system("")
+
+    # Start the program
+    main()
