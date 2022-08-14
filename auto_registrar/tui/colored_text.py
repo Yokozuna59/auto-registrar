f5a4731ef63bccf6ffbadf98fd5880489906e7e7
Yokozuna59 <u.yokozuna@gmail.com>
2022-08-14 05:31:59 +0300

    udpate RESET variable to RESET_ALL
    Signed-off-by: Yokozuna59 <u.yokozuna@gmail.com>

--- auto_registrar/tui/colored_text.py
+++ auto_registrar/tui/colored_text.py
@@ -4,7 +4,7 @@ from auto_registrar.tui.ansi import AnsiKeys, AnsiStyle
 
 
 def print_one_color_text(text_string: str, text_color: str, end_with=AnsiKeys.NEW_LINE):
-    stdout.write(f"{text_color}{text_string}{AnsiStyle.RESET}{end_with}")
+    stdout.write(f"{text_color}{text_string}{AnsiStyle.RESET_ALL}{end_with}")
     stdout.flush()
 
 
@@ -13,7 +13,7 @@ def print_more_color_text(*args, end_with=AnsiKeys.NEW_LINE):
     while len(args) > index:
         text_string = args[index]
         text_color = args[index + 1]
-        stdout.write(f"{text_color}{text_string}{AnsiStyle.RESET} ")
+        stdout.write(f"{text_color}{text_string}{AnsiStyle.RESET_ALL} ")
         index += 2
     stdout.write(end_with)
     stdout.flush()
