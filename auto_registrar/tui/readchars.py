0a56edd62fe154514d8465a1497756281429c261
Yokozuna59 <u.yokozuna@gmail.com>
2022-08-22 22:43:26 +0300

    sample echo mode
    Signed-off-by: Yokozuna59 <u.yokozuna@gmail.com>

--- auto_registrar/tui/readchars.py
+++ auto_registrar/tui/readchars.py
@@ -1,4 +1,5 @@
 from concurrent.futures import ThreadPoolExecutor, TimeoutError
+from sys import stdout
 
 try:
     from sys import stdin
@@ -44,3 +45,49 @@ def read_more_chars() -> str:
     if chars[-1].isdigit():
         chars += stdin.read(1)
     return chars
+
+
+# def echo_mode_off() -> tuple:
+#     fd = stdin.fileno()
+#     if unix:
+#         old_settings = tcgetattr(fd)
+#         setraw(fd=fd)
+#     else:
+#         pass
+
+#     return fd, old_settings
+
+
+# def echo_mode_on(fd: int, old_settings: list) -> None:
+#     if unix:
+#         tcsetattr(fd, TCSADRAIN, old_settings)
+#     else:
+#         pass
+
+
+# def read_one_char() -> str:
+#     if unix:
+#         char = stdin.read(1)
+#         if char == "\x1B":
+#             executor = ThreadPoolExecutor()
+#             future = executor.submit(read_more_chars)
+#             try:
+#                 return_value = future.result(timeout=0.01)
+#                 future.cancel()
+#                 char += return_value
+#             except TimeoutError:
+#                 future.cancel()
+#                 stdout.flush()
+#                 stdin.flush()
+
+#     else:
+#         if kbhit():
+#             char = getch()
+#     return char
+
+
+# fd, old = echo_mode_off()
+# tt = ""
+# while True:
+#     tt += read_one_char()
+# echo_mode_on(fd, old)
